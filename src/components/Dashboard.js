511f2ad11214e4e743fcfe226559af5b381b193a
AramisB <brendachem7@gmail.com>
2024-10-23 04:23:23 +0300

    Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,7 +14,7 @@ const Dashboard = () => {
     if (user) {
       const fetchData = async () => {
         try {
-          const response = await axios.get(`/api/dashboard`, {
+          const response = await axios.get(`https://monkfish-app-od5cu.ondigitalocean.app/api/dashboard`, {
             headers: {
               Authorization: `Bearer ${user.token}`,
             },
